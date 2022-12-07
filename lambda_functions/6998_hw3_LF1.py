ac91a0d0aa255d73f75fa6baf348cd12dc58d02c
Maizer <qpalzmxdrfyved@gmail.com>
2022-12-07 16:56:33 -0500

    updated sagemaker part

--- lambda_functions/6998_hw3_LF1.py
+++ lambda_functions/6998_hw3_LF1.py
@@ -1,11 +1,17 @@
 import json
-import logging
 import boto3
+import logging
+import email
+
+from sms_spam_classifier_utilities import one_hot_encode
+from sms_spam_classifier_utilities import vectorize_sequences
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+
 def lambda_handler(event, context):
+    
     logger.info(event)
 
     # validate event
@@ -20,23 +26,69 @@ def lambda_handler(event, context):
             'body': e
         }
 
-    email = None
-    with boto3.resource('s3') as s3:
-        email = s3.Object(bucket, filename)
-        email.get()['Body'].read().decode('utf-8')
-    if email is None:
-        logger.error("Failed to retrieve email content from s3")
-    else: 
-        logger.info(email)
+    # retrieve and parse email file
+    s3 = boto3.resource('s3')
+    file = s3.Object(bucket, filename)
+    file = file.get()['Body'].read()
+    file = email.message_from_bytes(file)
+    if file.is_multipart():
+        for _file in file.get_payload():
+            msg = _file.get_payload()
+            break
+    else:
+        msg = file.get_payload
     
-    # connect to sagemaker endpoint
+    
+    # sagemaker
     client = boto3.client('sagemaker-runtime')
-    response = client.invoke_endpoint(
-        EndpointName='sms-spam-classifier-mxnet-2022-12-04-19-08-20-432',
-        Body=email
+    
+    try:
+        response = client.invoke_endpoint(
+            EndpointName='sms-spam-classifier-mxnet-2022-12-04-19-08-20-432',
+            Body=vectorize_sequences( one_hot_encode(msg, 9013), 9013 )
+        )
+        response = response['Body'].read().decode()
+        logger.info("Response body from model endpoint: {}".format(response))
+    except Exception as e:
+        logger.error("Unable to parse model response")
+        return {
+            'statusCode': 500,
+            'body': e
+        }
+        
+    
+    # send responding email back to sender
+    ses = boto3.client('ses')
+    CHARSET = "UTF-8"
+    
+    content = """
+    We received your email sent at {} with the subject {}.
+    
+    Here is a 240 character sample of the email body: {}
+    
+    The email was categorized as {} with a {}% confidence.
+    """.format( t, file['subject'], msg[:240], 'N/A', 'N/A' )
+    
+    logger.info(content)
+    
+    res = ses.send_email(
+        Source='admin@spam.maizer.pw',
+        Destination={
+            'ToAddresses': [file['from']],
+        },
+        Message={
+            'Subject': {
+                'Data': 'email recieved',
+                'Charset': CHARSET
+            },
+            'Body': {
+                'Text': {
+                    'Data': content,
+                    'Charset': CHARSET
+                }
+            }
+        }
     )
-
-    logger.info(response)
 
 
     return {
